dd013ec9ca4a670bb671df950f1b3bda1e66b75b
docNemo <varyag300300@yandex.ru>
2023-10-19 02:31:29 +0300

    feature/devslaves-971395 : add logger

--- arachni-articles-object-storage/src/main/java/ru/mai/arachni/articles/objectstorage/scheduler/ObjectStorageScheduler.java
+++ arachni-articles-object-storage/src/main/java/ru/mai/arachni/articles/objectstorage/scheduler/ObjectStorageScheduler.java
@@ -1,17 +1,20 @@
 package ru.mai.arachni.articles.objectstorage.scheduler;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import ru.mai.arachni.articles.objectstorage.service.ObjectStorageService;
 
 import java.util.concurrent.TimeUnit;
-
+@Slf4j
 @RequiredArgsConstructor
 public class ObjectStorageScheduler {
     private final ObjectStorageService objectStorageService;
 
     @Scheduled(fixedDelay = 10, timeUnit = TimeUnit.MINUTES)
     public void uploadAllTempText() {
+        LOGGER.info("start transfer article's texts");
         objectStorageService.uploadAllTempText();
+        LOGGER.info("successful transfer article's texts");
     }
 }

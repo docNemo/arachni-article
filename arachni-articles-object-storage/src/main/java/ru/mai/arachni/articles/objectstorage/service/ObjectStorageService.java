755766c559452f53aa02d178dceeb1c5f90d19b6
docNemo <varyag300300@yandex.ru>
2023-10-19 03:07:22 +0300

    feature/devslaves-971395 : add pagination

--- arachni-articles-object-storage/src/main/java/ru/mai/arachni/articles/objectstorage/service/ObjectStorageService.java
+++ arachni-articles-object-storage/src/main/java/ru/mai/arachni/articles/objectstorage/service/ObjectStorageService.java
@@ -1,10 +1,15 @@
 package ru.mai.arachni.articles.objectstorage.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+import ru.mai.arachni.articles.core.domain.TempText;
 import ru.mai.arachni.articles.core.repository.TempTextRepository;
 import ru.mai.arachni.articles.objectstorage.provider.ObjectStorageProvider;
 
+@Slf4j
 @RequiredArgsConstructor
 public class ObjectStorageService {
     private final ObjectStorageProvider objectStorageProvider;
@@ -12,17 +17,20 @@ public class ObjectStorageService {
 
     @Transactional
     public void uploadAllTempText() {
-        tempTextRepository
-                .findAll()
-                .parallelStream()
-                .forEach(
-                        tempText -> {
-                            objectStorageProvider.uploadArticleText(
-                                    tempText.getFileName(),
-                                    tempText.getText()
-                            );
-                            tempTextRepository.deleteById(tempText.getId());
-                        }
-                );
+        Page<TempText> page = null;
+        do {
+            page = tempTextRepository
+                    .findAll(Pageable.ofSize(25));
+            page.forEach(
+                    tempText -> {
+                        objectStorageProvider.uploadArticleText(
+                                tempText.getFileName(),
+                                tempText.getText()
+                        );
+                        tempTextRepository.deleteById(tempText.getId());
+                    }
+            );
+            LOGGER.info("transfer {} texts", page.getSize());
+        } while (page.hasNext());
     }
 }
